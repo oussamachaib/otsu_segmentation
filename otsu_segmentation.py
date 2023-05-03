58f169a8a07658b5cca442ef75ab4684cb40da00
Oussama <88498674+oussamachaib@users.noreply.github.com>
2023-05-03 11:59:52 +0100

    corrections
    Two mains modifications :
    1) First order statistical moment implementation based on maximizing the inter-class variance (instead of intra-class) = order of magnitude improvement in computation time in addition to being shorter.
    2) Fixed issue with accessing indices of binarized image (and anti-image).

--- otsu_segmentation.py
+++ otsu_segmentation.py
@@ -7,19 +7,22 @@ def otsu(img):
     img = 255*(img/np.max(img))
     img = img.astype(np.uint8)
 
-    sigma = np.zeros(256)
-
-    for i in np.arange(0,256,1):
+    var = np.zeros(256)
+    
+    # Using the inter-class variance (first statistical moments only)
+    for i in np.arange(1,255,1):
         # Thresholding the image using grayscale value i
         binarized_image = img>i
         # Computing conditional statistics
-        std_foreground = np.nanstd(img[binarized_image])
-        std_background = np.nanstd(img[1-binarized_image])
         cdf_foreground = np.sum(binarized_image)/np.size(img)
         cdf_background = 1-cdf_foreground
+        mean_foreground = (np.mean(img[binarized_image == 1]))
+        mean_background = (np.mean(img[binarized_image == 0]))
         # Computing the intra-class variance
-        sigma[i] =  np.sqrt(cdf_background*std_background**2 + cdf_foreground*std_foreground**2)
-
-    otsu_threshold = int(np.nanargmin(sigma))
+        var[i] =  cdf_background*cdf_foreground*(mean_background-mean_foreground)**2
+    
+    
+    # Determining the Otsu threshold (the last element is discarded due to empty foreground array)
+    otsu_threshold = int(np.nanargmax(var[0:-2]))
 
     return otsu_threshold
